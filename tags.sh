e762ecb1c6907fa5e89a2d2171d135cf13a96e3e
Marc-André Faucher <ma.faucher@gmail.com>
2013-08-07 13:28:40 -0400

    added tags support for vim

---
+++ tags.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Index java source
+ctags -R -f $HOME/.vim/tags/java $HOME/Public/tools/commons-lang3-3.1/src/ $HOME/Public/tools/commons-io-2.4/src/ $HOME/Public/tools/lucene-4.1.0/core/src/ $HOME/Public/tools/lucene-4.1.0/queryparser/src/ $HOME/Public/tools/lucene-4.1.0/analysis/common/src/ $HOME/Public/tools/java-6-src/
